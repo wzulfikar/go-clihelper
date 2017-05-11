f7c21c751e90c5bb138c6e8f0f8dd5d6b0c9b388
wzulfikar_mba <me@wzulfikar.com>
2017-05-12 07:24:15 +0800

    Initial commit

---
+++ clihelper.go
@@ -0,0 +1,41 @@
+package clihelper
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/fatih/color"
+)
+
+var ExitHeader = "Whoops! Something went wrong :("
+var ExitMsg = "\nPress enter to exit.."
+
+func ExitWithMessage(msg string, errorCode int) {
+	if errorCode > 0 && len(ExitHeader) > 0 {
+		oops := fmt.Sprintf("%s\n", color.RedString(ExitHeader))
+
+		if runtime.GOOS == "windows" {
+			fmt.Fprintf(color.Output, oops)
+		} else {
+			fmt.Printf(oops)
+		}
+	}
+
+	fmt.Println(msg)
+	Pause(ExitMsg)
+
+	os.Exit(errorCode)
+}
+
+func CheckError(err error) {
+	if err != nil {
+		ExitWithMessage(err.Error(), 1)
+	}
+}
+
+func Pause(msg string) {
+	fmt.Println(msg)
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
